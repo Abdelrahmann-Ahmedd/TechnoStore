1eb2fb9fc57ad53fcc47ff39073229ca71b03db8
Abdelrahmann-Ahmedd <abdelrahman.w164@gmail.com>
2025-12-24 23:16:39 +0200

    enhance image priority and loading

--- app/(admin)/admin/order/page.tsx
+++ app/(admin)/admin/order/page.tsx
@@ -26,8 +26,8 @@ export default function AllOrder() {
     <div className="container my-5">
       <h2 className="text-center mb-4 fw-bold">All Orders</h2>
       <div className="d-flex flex-column gap-4">
-        {allOrders.map((order) => (
-          <OrderCard key={order._id} order={order} />
+        {allOrders.map((order,ind) => (
+          <OrderCard key={order._id} order={order} index={ind} />
         ))}
       </div>
     </div>
